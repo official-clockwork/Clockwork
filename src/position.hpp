eb0b3921843fb2b129f2172239831be97881dcbd
87 <dev@87flowers.com>
2025-06-07 14:08:25 +0100

    add [[nodiscard]]

--- src/position.hpp
+++ src/position.hpp
@@ -112,7 +112,7 @@ public:
 
     [[nodiscard]] Position move(Move m) const;
 
-    std::tuple<Wordboard, Bitboard> calc_pin_mask() const;
+    [[nodiscard]] std::tuple<Wordboard, Bitboard> calc_pin_mask() const;
 
     const std::array<Wordboard, 2> calc_attacks_slow();
     const std::array<u16, 2>       calc_attacks_slow(Square sq);
