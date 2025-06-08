98f6a0add7469f067fde06582a4768f2627a9bc8
TheRealGioviok <64363733+TheRealGioviok@users.noreply.github.com>
2025-06-09 00:09:12 +0200

    Flatten Clockwork::Bench namespace in bench file
    bench 68854202
    
    Co-authored-by: 87 <dev@87flowers.com>

--- src/bench.cpp
+++ src/bench.cpp
@@ -7,8 +7,7 @@
 #include <iostream>
 #include <string>
 
-namespace Clockwork {
-namespace Bench {
+namespace Clockwork::Bench {
 const std::array<std::string, 50> BENCH_FENS = {
   {"r3k2r/2pb1ppp/2pp1q2/p7/1nP1B3/1P2P3/P2N1PPP/R2QK2R w KQkq a6 0 14",
    "4rrk1/2p1b1p1/p1p3q1/4p3/2P2n1p/1P1NR2P/PB3PP1/3R1QK1 b - - 2 24",
