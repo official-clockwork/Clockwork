5b2547a83b365c1d97f130f406670f773983cc28
Giovanni Maria Manduca <425gioviok@gmail.com>
2025-06-10 15:58:03 +0200

    RepetitionInfo classes

---
+++ src/repetition_info.hpp
@@ -0,0 +1,20 @@
+#include "common.hpp"
+#include "util/types.hpp"
+#include <array>
+
+namespace Clockwork {
+class RepetitionInfo {
+public:
+    RepetitionInfo();
+    void push(HashKey key, bool is_reversible);
+    void pop();
+    void reset();
+
+    bool detect_repetition(size_t root_ply);
+
+private:
+    std::array<std::pair<HashKey, bool>, MAX_PLY * 2> m_repetition_table;
+    size_t                                            m_index;
+    size_t                                            m_ply_from_null;
+};
+}
