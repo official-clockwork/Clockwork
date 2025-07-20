c911b1b512606f4c2ed6dd6af64da76bfd5e712a
Eren Araz <lerenarazl@gmail.com>
2025-07-20 17:13:00 +0300

    bench: 1497824

--- src/search.cpp
+++ src/search.cpp
@@ -255,8 +255,7 @@ Value Worker::search(Position& pos, Stack* ss, Value alpha, Value beta, Depth de
             }
         }
         auto see_threshold = quiet ? -67 * depth : -32 * depth;
-        // SEE PVS Pruning
-        
+        // SEE PVS Pruning        
         if (depth <= 10 && !ROOT_NODE && !SEE::see(pos, m, see_threshold)) {
             continue;
         }
