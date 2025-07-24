ce179d7e7278a47f189b7d91621aff21ba96f5d9
TheRealGioviok <425gioviok@gmail.com>
2025-07-24 15:29:42 +0200

    test tuning

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,11 @@
 #include "uci.hpp"
 #include "util/types.hpp"
 #include "zobrist.hpp"
+#include "position.hpp"
 #include <functional>
 #include <tuple>
+#include <iostream>
+#include <fstream>
 
 using namespace Clockwork;
 
@@ -26,88 +29,107 @@ int main(int argc, char* argv[]) {
 
 
     // Load fens.
-    // std::vector<std::string> fens;
-    // std::vector<f64>         results;
-    // std::ifstream            fenFile("fens.book");
-    // if (!fenFile) {
-    //     std::cerr << "Error opening fens.book" << std::endl;
-    //     return 1;
-    // };
-    // std::string line;
-    // while (std::getline(fenFile, line)) {
-    //     // The line is : FEN;(w/d/b)
-    //     size_t pos = line.find(';');
-    //     if (pos != std::string::npos) {
-    //         std::string fen = line.substr(0, pos);
-    //         fens.push_back(fen);
-    //         std::string result = line.substr(pos + 1);
-    //         if (result == "w") {
-    //             results.push_back(1.0);  // Win
-    //         } else if (result == "d") {
-    //             results.push_back(0.5);  // Draw
-    //         } else if (result == "b") {
-    //             results.push_back(0.0);  // Loss
-    //         } else {
-    //             std::cerr << "Invalid result in line: " << line << std::endl;
-    //         }
-    //     } else {
-    //         std::cerr << "Invalid line format: " << line << std::endl;
-    //     }
-    // }
-    // fenFile.close();
+    std::vector<std::string> fens;
+    std::vector<f64>         results;
+    std::ifstream            fenFile("fens.book");
+    if (!fenFile) {
+        std::cerr << "Error opening fens.book" << std::endl;
+        return 1;
+    };
+    std::string line;
+    while (std::getline(fenFile, line)) {
+        // The line is : FEN;(w/d/b)
+        size_t pos = line.find(';');
+        if (pos != std::string::npos) {
+            std::string fen = line.substr(0, pos);
+            fens.push_back(fen);
+            std::string result = line.substr(pos + 1);
+            if (result == "w") {
+                results.push_back(1.0);  // Win
+            } else if (result == "d") {
+                results.push_back(0.5);  // Draw
+            } else if (result == "b") {
+                results.push_back(0.0);  // Loss
+            } else {
+                std::cerr << "Invalid result in line: " << line << std::endl;
+            }
+        } else {
+            std::cerr << "Invalid line format: " << line << std::endl;
+        }
+    }
+    fenFile.close();
 
-    // // Print the number of fens loaded
-    // std::cout << "Loaded " << fens.size() << " FENs." << std::endl;
+    // Print the number of fens loaded
+    std::cout << "Loaded " << fens.size() << " FENs." << std::endl;
 
-    auto a = Clockwork::Autograd::Value<f64>::create_tunable(0);
-    auto b = Clockwork::Autograd::Value<f64>::create_tunable(0);
-    auto c = Clockwork::Autograd::Value<f64>::create_tunable(0);
-    auto d = Clockwork::Autograd::Value<f64>::create_tunable(0);
+    auto PAWN_MAT = Clockwork::Autograd::Value<f64>::create_tunable(0);
+    auto KNIGHT_MAT = Clockwork::Autograd::Value<f64>::create_tunable(0);
+    auto BISHOP_MAT = Clockwork::Autograd::Value<f64>::create_tunable(0);
+    auto ROOK_MAT = Clockwork::Autograd::Value<f64>::create_tunable(0);
+    auto QUEEN_MAT = Clockwork::Autograd::Value<f64>::create_tunable(0);
+    auto MOBILITY_VAL = Clockwork::Autograd::Value<f64>::create_tunable(0);
+    auto TEMPO_VAL = Clockwork::Autograd::Value<f64>::create_tunable(0);
 
-    Clockwork::Autograd::Graph<f64>::get()->register_param(a);
-    Clockwork::Autograd::Graph<f64>::get()->register_param(b);
-    Clockwork::Autograd::Graph<f64>::get()->register_param(c);
-    Clockwork::Autograd::Graph<f64>::get()->register_param(d);
+    Clockwork::Autograd::Graph<f64>::get()->register_param(PAWN_MAT);
+    Clockwork::Autograd::Graph<f64>::get()->register_param(KNIGHT_MAT);
+    Clockwork::Autograd::Graph<f64>::get()->register_param(BISHOP_MAT);
+    Clockwork::Autograd::Graph<f64>::get()->register_param(ROOK_MAT);
+    Clockwork::Autograd::Graph<f64>::get()->register_param(QUEEN_MAT);
+    Clockwork::Autograd::Graph<f64>::get()->register_param(MOBILITY_VAL);
+    Clockwork::Autograd::Graph<f64>::get()->register_param(TEMPO_VAL);
 
-    auto tunable_function = [a, b, c, d](f64 x, f64 y) {
-        return std::pow(x, 2) * a + x * y * b + y * c + d;
+    auto tunable_function = [PAWN_MAT, KNIGHT_MAT, BISHOP_MAT, ROOK_MAT, QUEEN_MAT, MOBILITY_VAL, TEMPO_VAL](std::string fen) {
+        auto pos = Position::parse(fen).value();
+
+        const Color us   = pos.active_color();
+        const Color them = invert(us);
+
+        auto material =
+          PAWN_MAT * static_cast<f64>(pos.piece_count(Color::White, PieceType::Pawn) - pos.piece_count(Color::Black, PieceType::Pawn))
+          + KNIGHT_MAT * static_cast<f64>(pos.piece_count(Color::White, PieceType::Knight) - pos.piece_count(Color::Black, PieceType::Knight))
+          + BISHOP_MAT * static_cast<f64>(pos.piece_count(Color::White, PieceType::Bishop) - pos.piece_count(Color::Black, PieceType::Bishop))
+          + ROOK_MAT * static_cast<f64>(pos.piece_count(Color::White, PieceType::Rook) - pos.piece_count(Color::Black, PieceType::Rook))
+          + QUEEN_MAT * static_cast<f64>(pos.piece_count(Color::White, PieceType::Queen) - pos.piece_count(Color::Black, PieceType::Queen));
+
+        auto mobility = Clockwork::Autograd::Value<f64>::create(0);
+        for (u64 x : std::bit_cast<std::array<u64, 16>>(pos.attack_table(Color::White))) {
+            mobility = mobility + MOBILITY_VAL * static_cast<f64>(std::popcount(x));
+        }
+        for (u64 x : std::bit_cast<std::array<u64, 16>>(pos.attack_table(Color::Black))) {
+            mobility = mobility - MOBILITY_VAL * static_cast<f64>(std::popcount(x));
+        }
+
+        auto tempo = (us == Color::White)? TEMPO_VAL : -TEMPO_VAL;
+
+        return material + mobility + tempo;
     };
 
     auto                          loss_fn = Clockwork::Autograd::mse<f64>;
-    Clockwork::Autograd::SGD<f64> optim(Clockwork::Autograd::Graph<f64>::get()->get_parameters(),
-                                        0.1, 0.9);
-
-    std::vector<std::tuple<f64, f64, f64>> data;
-    const f64                              true_a = 3;
-    const f64                              true_b = -2;
-    const f64                              true_c = 1;
-    const f64                              true_d = 5;
-
-    // Simple 2D grid sampling
-    for (f64 x = -5.0; x <= 5.0; x += 1.0) {
-        for (f64 y = -5.0; y <= 5.0; y += 1.0) {
-            f64 z = true_a * x * x + true_b * x * y + true_c * y + true_d;
-            data.emplace_back(x, y, z);
-        }
-    }
+    Clockwork::Autograd::SGD<f64> optim(Clockwork::Autograd::Graph<f64>::get()->get_parameters(), 0.1, 0.9);
 
     i32 epochs = 10;
-
+    const f64 K = 1.0 / 650;
     for (i32 epoch = 0; epoch < epochs; epoch++) {
-        for (auto datapoint : data) {
-            f64 x      = std::get<0>(datapoint);
-            f64 y      = std::get<1>(datapoint);
-            f64 target = std::get<2>(datapoint);
+        for (size_t i = 0; i < fens.size(); i++){
+            std::string fen      = fens[i];
+            f64 y      = results[i];
 
-            auto result = tunable_function(x, y);
+            auto result = (tunable_function(fen) * K)->sigmoid();
 
-            auto loss = loss_fn(std::vector({result}), std::vector({target}));
+            auto loss = loss_fn(std::vector({result}), std::vector({y}));
 
             Clockwork::Autograd::Graph<f64>::get()->backward();
+            
             optim.step();
+
+            for (auto param : Clockwork::Autograd::Graph<f64>::get()->get_parameters()) {
+                std::cout << param << std::endl;
+            }
+            std::cout << std::endl;
+
             Clockwork::Autograd::Graph<f64>::get()->cleanup();
         }
-        std::cout << "Values: " << a << " " << b << " " << c << " " << d << std::endl;
+        
     }
 
     return 0;
