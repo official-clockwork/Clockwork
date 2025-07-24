0ccb57979b0ed68dfc0f0c7a2c2e03a0df7930bc
TheRealGioviok <425gioviok@gmail.com>
2025-07-24 03:29:28 +0200

    format

--- src/tuning/dataset.hpp
+++ src/tuning/dataset.hpp
@@ -3,19 +3,18 @@
 #include "graph.hpp"
 #include "value.hpp"
 #include <memory>
-#include <vector>
 #include <string>
+#include <vector>
 
 namespace Clockwork {
 namespace Autograd {
-    class Dataset {
-        private:
-        std::vector<std::string> m_lines;
+class Dataset {
+private:
+    std::vector<std::string> m_lines;
 
-        public:
-        Dataset(std::string load_dir) : m_lines({}){};
-
-
-    }
+public:
+    Dataset(std::string load_dir) :
+        m_lines({}) {};
+}
 }
 }
