8b543c7212663e231f681e9a3cf98c525c80f428
TheRealGioviok <425gioviok@gmail.com>
2025-08-25 03:31:50 +0200

    scaled params up (x2.72)

--- src/search.cpp
+++ src/search.cpp
@@ -332,7 +332,7 @@ Value Worker::search(
     }
 
     if (!PV_NODE && !is_in_check && depth >= tuned::nmp_depth && tt_adjusted_eval >= beta) {
-        int      R         = tuned::nmp_base_r + std::min(3, (tt_adjusted_eval - beta) / 300);
+        int      R         = tuned::nmp_base_r + std::min(3, (tt_adjusted_eval - beta) / 816);
         Position pos_after = pos.null_move();
 
         repetition_info.push(pos_after.get_hash_key(), true);
@@ -348,7 +348,7 @@ Value Worker::search(
     }
 
     // Razoring
-    if (!PV_NODE && !is_in_check && depth <= 7 && static_eval + 260 * depth < alpha) {
+    if (!PV_NODE && !is_in_check && depth <= 7 && static_eval + 707 * depth < alpha) {
         const Value razor_score = quiesce<IS_MAIN>(pos, ss, alpha, beta, ply);
         if (razor_score <= alpha) {
             return razor_score;
