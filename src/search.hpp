0cca414348e915dfd7369caf9699c8b93d7923b3
BuildTools <anikpatel1322@gmail.com>
2025-09-05 23:11:24 -0400

    int to i32

--- src/search.hpp
+++ src/search.hpp
@@ -38,7 +38,7 @@ struct Stack {
     Move*          pv;
     Move           killer;
     ContHistEntry* cont_hist_entry;
-    int            fail_high_count;
+    i32            fail_high_count;
 };
 
 struct SearchLimits {
