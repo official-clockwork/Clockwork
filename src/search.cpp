581f14e379e85ef36fb1b4e0f60af6854596527f
Eren Araz <lerenarazl@gmail.com>
2025-10-05 20:29:31 +0300

    bench: 17732746

--- src/search.cpp
+++ src/search.cpp
@@ -512,10 +512,9 @@ Value Worker::search(
                 // Double Extension
                 if (!PV_NODE && singular_value <= singular_beta - 40) {
                     extension = 2;
-                }
-                
+                }                
             }
-            
+            // Negative Extensions
             else if (tt_data->score >= beta) {
                 extension = -1;
             }
