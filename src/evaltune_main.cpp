5ce5d6ee11af3854596fbb351419b3f5a5253b92
mcthouacbb <mcthouacbb@gmail.com>
2025-10-18 13:57:58 -0400

    Add v3 + retune from zero Bench: 13050727

--- src/evaltune_main.cpp
+++ src/evaltune_main.cpp
@@ -31,10 +31,8 @@ int main() {
 
     // List of files to load
     const std::vector<std::string> fenFiles = {
-      "data/dfrcv1/dfrc-1m.txt",
-      "data/dfrcv0/v0.txt",
-      "data/v2.2/filtered_data.txt",
-      "data/v2.1/filtered_data.txt",
+      "data/dfrcv1/dfrc-1m.txt",     "data/dfrcv0/v0.txt", "data/v2.2/filtered_data.txt",
+      "data/v2.1/filtered_data.txt", "data/v3/v3.txt",
     };
 
     // Number of threads to use, default to half available
