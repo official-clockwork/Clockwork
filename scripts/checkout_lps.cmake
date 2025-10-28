da6e800512f27db45286b17101048c5c1194025b
87 <dev@87flowers.com>
2025-10-28 15:27:56 +0000

    Bench: 11239275

--- scripts/checkout_lps.cmake
+++ scripts/checkout_lps.cmake
@@ -11,6 +11,23 @@ file(READ "${CMAKE_CURRENT_LIST_DIR}/../vendor/lps_commit.txt" LPS_COMMIT)
 string(STRIP ${LPS_COMMIT} LPS_COMMIT)
 
 message(STATUS "Checking out lps commit ${LPS_COMMIT}...")
+
+execute_process(
+    COMMAND ${GIT_EXECUTABLE} show ${LPS_COMMIT}
+    WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/../vendor/lps"
+    RESULT_VARIABLE COMMIT_NOT_PRESENT
+    OUTPUT_QUIET
+    ERROR_QUIET)
+
+if (COMMIT_NOT_PRESENT)
+    message(STATUS "Fetching from lps upstream...")
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} fetch
+        WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/../vendor/lps"
+        COMMAND_ERROR_IS_FATAL ANY)
+endif ()
+
 execute_process(
     COMMAND ${GIT_EXECUTABLE} -c advice.detachedHead=false checkout ${LPS_COMMIT}
-    WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/../vendor/lps")
+    WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/../vendor/lps"
+    COMMAND_ERROR_IS_FATAL ANY)
