af233189737620895afd1f3ade8add5f3e9340bc
Eren Araz <lerenarazl@gmail.com>
2025-12-03 04:42:44 +0300

    professor dr. extension please cook

--- src/search.cpp
+++ src/search.cpp
@@ -629,7 +629,9 @@ Value Worker::search(
                 }
 
                 // Triple Extension
-                if (!PV_NODE && quiet && singular_value <= singular_beta - 120) {
+                int triple_margin = 120 - (move_history / 512 * quiet);
+                
+                if (!PV_NODE && quiet && singular_value <= singular_beta - triple_margin) {
                     extension = 3;
                 }
             }
