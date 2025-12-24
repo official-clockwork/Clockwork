6944595aaeb5c68b3977303ff5b03b9597fb73b4
Eren Araz <lerenarazl@gmail.com>
2025-12-24 15:37:02 +0300

    glad to hear, now sit here bench: 12447864

--- src/search.cpp
+++ src/search.cpp
@@ -642,7 +642,6 @@ Value Worker::search(
                     && singular_value <= singular_beta - tuned::sing_triple_margin) {
                     extension = 3;
                 }
-                
             }
 
             // Multicut
