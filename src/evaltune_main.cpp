6262883887f092a7c2eabd73d560f17b78e2d0a1
TheRealGioviok <425gioviok@gmail.com>
2026-01-24 13:33:42 +0100

    Bench: 14488986

--- src/evaltune_main.cpp
+++ src/evaltune_main.cpp
@@ -37,8 +37,9 @@ int main() {
     std::vector<f64>      results;
 
     const std::vector<std::string> fenFiles = {
-      "data/dfrcv1.txt",   "data/dfrcv0.txt",    "data/v3.txt",         "data/v4_5knpm.txt",
-      "data/v4_8knpm.txt", "data/v4_16knpm.txt", "data/v4.1_5knpm.txt", "data/v4.1_8knpm.txt", "data/combined.txt",
+      "data/dfrcv1.txt",     "data/dfrcv0.txt",     "data/v3.txt",
+      "data/v4_5knpm.txt",   "data/v4_8knpm.txt",   "data/v4_16knpm.txt",
+      "data/v4.1_5knpm.txt", "data/v4.1_8knpm.txt", "data/combined.txt",
     };
 
     const u32 thread_count = std::max<u32>(1, std::thread::hardware_concurrency() / 2);
